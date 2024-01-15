00b8ed12496f26ed6a15ba577a5637a4db508a71
Mohit <mohit@hey.com>
2024-01-15 12:31:21 +0530

    x

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,13 @@
 import { defineConfig } from 'astro/config';
 
+import cloudflare from "@astrojs/cloudflare";
+import mdx from "@astrojs/mdx";
+import tailwind from "@astrojs/tailwind";
+import react from "@astrojs/react";
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  output: "server",
+  adapter: cloudflare(),
+  integrations: [mdx(), tailwind(), react()]
+});
