15dfbc96a8d8191b5f0f8d9bcd2ebe854365cc58
Kunal Singh <kunalsin9h@gmail.com>
2024-01-15 16:14:57 +0530

    feat: header
    Signed-off-by: Kunal Singh <kunalsin9h@gmail.com>

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,14 @@
 import { defineConfig } from 'astro/config';
-
 import cloudflare from "@astrojs/cloudflare";
 import mdx from "@astrojs/mdx";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 
+import icon from "astro-icon";
+
 // https://astro.build/config
 export default defineConfig({
   output: "server",
   adapter: cloudflare(),
-  integrations: [mdx(), tailwind(), react()]
+  integrations: [mdx(), tailwind(), react(), icon()]
 });
