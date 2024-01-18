742aabd7d377b495c7a3cdebaf87ba42e4d84e93
Kunal Singh <kunalsin9h@gmail.com>
2024-01-18 20:13:46 +0530

    fix(layout, using consts to set title, image, and decription
    Signed-off-by: Kunal Singh <kunalsin9h@gmail.com>

--- src/constants/website.js
+++ src/constants/website.js
@@ -1,1 +1,3 @@
 export const WEBSITE = "https://c43d.blupp.co/"
+export const FAVICON = "https://www.c42d.com/wp-content/uploads/2022/03/cropped-2022-favicon-32x32.png"
+export const OG_IMAGE = "https://www.c42d.com/wp-content/uploads/2023/08/C42D-YELLOW-ffd541-RGB-01-1.png"
